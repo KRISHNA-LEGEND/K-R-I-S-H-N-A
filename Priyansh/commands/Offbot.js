dceec43927e25040caf41a700cbfce7c5ac32ac7
KRISHNA-LEGEND <rajkumar9649563706@gmail.com>
2025-03-08 21:05:23 +0530

    Update Offbot.js

--- Priyansh/commands/Offbot.js
+++ Priyansh/commands/Offbot.js
@@ -9,6 +9,6 @@ module.exports.config = {
         };
 module.exports.run = ({event, api}) =>{
     const permission = ["100062282613155", "100062282613155"];
-  	if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] OYE YE COMMAND SHAAN BOSS DE SAK THA AAI SAMJH🙄", event.threadID, event.messageID);
+  	if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] You don't have permission to use this command, This Command Only For Priyansh", event.threadID, event.messageID);
   api.sendMessage(`[ OK ] ${global.config.BOTNAME} Bot are now turned off.`,event.threadID, () =>process.exit(0))
 }
